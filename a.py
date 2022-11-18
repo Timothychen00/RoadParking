110256bf9a4fa2bc6cdd9b0936a9c0ff9fea4593
TimHu950112 <tim20060112@gmail.com>
2022-11-18 23:06:51 +0800

    第二次上傳
    1.加入辨識區域匡選
    2.程式執行後，詢問檔名

--- a.py
+++ a.py
@@ -3,11 +3,17 @@ from imutils.perspective import four_point_transform
 import cv2,sys
 import numpy as np
 
-img_path = sys.argv[1]
+# img_path ="photo/"+sys.argv[1]
+img_path="photo/"+input("請輸入照片檔名(會自動填入路徑)：")
 image = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
 def transformation(points=get_points(img_path)):
     four_points =points
+    for i in range(0,3):
+        cv2.line(image, points[i], points[i+1], (0, 0, 255), 5)
+    cv2.line(image, points[0], points[3], (0, 0, 255), 5)
+    cv2.imshow("line", image)
+    print(points)
     rect = four_point_transform(image, np.array(four_points))
     cv2.imwrite('output.png', rect)
     cv2.imshow("rect", rect)
--- locate.py
+++ locate.py
@@ -9,8 +9,8 @@ def get_points(img):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
     gray = cv2.bilateralFilter(gray, 13, 15, 15)
     edged = cv2.Canny(gray, 30,150) #Perform Edge detection
-    cv2.imshow('edged',edged)
-    cv2.waitKey(0)
+    # cv2.imshow('edged',edged)
+    # cv2.waitKey(0)
 
     contours=cv2.findContours(edged.copy(),cv2.RETR_TREE,
                                                 cv2.CHAIN_APPROX_SIMPLE)
@@ -20,7 +20,7 @@ def get_points(img):
     for c in contours:
         # approximate the contour
         peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
         # if our approximated contour has four points, then
         # we can assume that we have found our screen
         if len(approx) == 4:
